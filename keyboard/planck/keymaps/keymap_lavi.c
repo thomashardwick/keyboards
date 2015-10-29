4348596ef0c270ad1e1b3d862fa267bef08f36c0
Thomas Hardwick <thomashauk@gmail.com>
2015-10-29 20:06:18 +0000

    Update to lavi keymap

--- keyboard/planck/keymaps/keymap_lavi.c
+++ keyboard/planck/keymaps/keymap_lavi.c
@@ -4,23 +4,23 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = { /* Qwerty */
-{KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
-{KC_MINS, KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
-{KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT},
-{KC_LCTL, KC_LALT, KC_LGUI, KC_APP, FUNC(2), KC_SPC,  KC_SPC,  FUNC(1), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+{KC_TAB,        KC_Q,          KC_W,    KC_E,   KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
+{KC_MINS,       KC_A,          KC_S,    KC_D,   KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
+{KC_LCTL,       KC_Z,          KC_X,    KC_C,   KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT},
+{LCTL(KC_LSFT), LCTL(KC_LALT), KC_LGUI, KC_APP, KC_LSFT, KC_SPC,  KC_SPC,  FUNC(1), FUNC(2), KC_RALT, KC_UP,   KC_LALT}
                                                 // Space is repeated to accommadate for both spacebar wiring positions
 },
 [1] = { /* RAISE */
-{KC_ESC,  KC_1,    KC_2,    KC_3,       KC_4,       KC_5,    KC_6,    KC_7,       KC_8,    KC_9,    KC_0,    KC_DEL},
-{KC_EQL,  S(KC_1), S(KC_2), S(KC_3),    S(KC_4),    S(KC_5), S(KC_6), S(KC_7),    S(KC_8), S(KC_9), S(KC_0), S(KC_EQL)},
-{KC_TRNS, KC_LBRC, KC_RBRC, S(KC_LBRC), S(KC_RBRC), KC_GRV,  KC_TRNS, S(KC_NUHS), S(KC_NUBS), KC_NUHS, KC_NUBS, KC_TRNS},
-{KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    FUNC(3),    KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS}
+{KC_ESC,  KC_1,    KC_2,    KC_3,      KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL},
+{KC_CAPS, KC_LEFT, KC_DOWN, KC_UP,     KC_RGHT, KC_PSCR, KC_SLCK, KC_PAUS, KC_NUHS, KC_LBRC, KC_RBRC, KC_EQL},
+{KC_TRNS, KC_HOME, KC_PGUP, KC_PGDOWN, KC_END,  KC_VOLU, KC_VOLD, KC_MUTE, KC_INS,  KC_GRV,  KC_NUBS, KC_TRNS},
+{KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, FUNC(3), KC_TRNS, KC_TRNS, KC_TRNS}
 },
 [2] = { /* LOWER */
-{KC_F1,   KC_F2,   KC_F3,   KC_F4,     KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12},
-{KC_SLCK, KC_HOME, KC_PGUP, KC_PGDOWN, KC_END,  KC_INS,  KC_PSCR, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PAUS},
-{KC_TRNS, KC_MNXT, KC_MPLY, KC_VOLD,   KC_VOLU, KC_MUTE, KC_CAPS, KC_BTN1, KC_BTN3, KC_BTN2, KC_WH_L, KC_TRNS},
-{KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, FUNC(3), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS}
+{KC_NLCK, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_P7,  KC_P8,    KC_P9,   LCTL(LALT(KC_P7)), LCTL(LALT(KC_P8)), LCTL(LALT(KC_P9)), KC_TRNS},
+{KC_TRNS, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_P4,  KC_P5,    KC_P6,   LCTL(LALT(KC_P4)), LCTL(LALT(KC_P5)), LCTL(LALT(KC_P6)), KC_TRNS},
+{KC_TRNS, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_P1,  KC_P2,    KC_P3,   LCTL(LALT(KC_P1)), LCTL(LALT(KC_P2)), LCTL(LALT(KC_P3)), KC_TRNS},
+{KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, FUNC(3), KC_TRNS,           KC_TRNS,           KC_TRNS,           KC_TRNS}
 },
 [3] = { /* RESET */
 {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET},
